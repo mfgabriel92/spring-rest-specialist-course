d9583fd7c1f7b8d64e8f1ab96f84f0aac8994ba6
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-11 20:41:38 -0300

    4.19. Defining HTTP response status with @ResponseStatus

--- src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
+++ src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
@@ -3,13 +3,13 @@ package br.gabriel.springrestspecialist.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.gabriel.springrestspecialist.api.model.KitchenXmlWrapper;
 import br.gabriel.springrestspecialist.domain.model.Kitchen;
 import br.gabriel.springrestspecialist.domain.repository.KitchenRepository;
 
@@ -24,11 +24,7 @@ public class KitchenController {
         return repository.findAll();
     }
     
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public KitchenXmlWrapper findAllXml() {
-        return new KitchenXmlWrapper(repository.findAll());
-    }
-    
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public Kitchen findById(@PathVariable Integer id) {
         return repository.findById(id);
