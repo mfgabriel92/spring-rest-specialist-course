7aa7bcf11f532ee09d8d9f9cc036fbb0ee4dec82
Gabriel M. Fernandes <gabriel.fernandes@labsit.io>
2020-06-11 10:55:18 -0300

    Initial commit

---
+++ src/main/java/br/gabriel/springrestspecialist/SpringRestSpecialistApplication.java
@@ -0,0 +1,13 @@
+package br.gabriel.springrestspecialist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSpecialistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSpecialistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gabriel/springrestspecialist/SpringRestSpecialistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gabriel.springrestspecialist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSpecialistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
