30dc41581ef43e869472ae4e460afd3d5b61fc8c
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-11 18:15:46 -0300

    4.10. Modeling and requesting a collection resource with GET

---
+++ src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
@@ -0,0 +1,23 @@
+package br.gabriel.springrestspecialist.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gabriel.springrestspecialist.domain.model.Kitchen;
+import br.gabriel.springrestspecialist.domain.repository.KitchenRepository;
+
+@RestController
+@RequestMapping("/kitchens")
+public class KitchenController {
+    @Autowired
+    private KitchenRepository repository;
+
+    @GetMapping
+    public List<Kitchen> findAll() {
+        return repository.findAll();
+    }
+}
