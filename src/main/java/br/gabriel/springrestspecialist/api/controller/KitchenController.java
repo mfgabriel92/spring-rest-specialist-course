f24cc11ae45afaa2b399b596c76827d132230602
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-11 18:46:30 -0300

    4.13. Implementing content negotiation to return either JSON or XML

--- src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
+++ src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
@@ -3,6 +3,7 @@ package br.gabriel.springrestspecialist.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,13 @@ public class KitchenController {
     @Autowired
     private KitchenRepository repository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Kitchen> findAll() {
+        return repository.findAll();
+    }
+    
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Kitchen> findAllXML() {
         return repository.findAll();
     }
 }
