5507021ae735f4fdbdf65fb0d08ae6b128a2148b
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-06-11 11:44:46 -0300

    3.4. Mapping entities with JPA

---
+++ src/main/java/br/gabriel/springrestspecialist/domain/model/Restaurant.java
@@ -0,0 +1,67 @@
+package br.gabriel.springrestspecialist.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+	@Id
+	private Integer id;
+
+	private String name;
+	
+	private BigDecimal shippingFee;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getShippingFee() {
+		return shippingFee;
+	}
+
+	public void setShippingFee(BigDecimal shippingFee) {
+		this.shippingFee = shippingFee;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurant other = (Restaurant) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
