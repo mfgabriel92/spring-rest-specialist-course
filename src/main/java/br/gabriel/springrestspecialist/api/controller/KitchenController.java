69aa3affa9005556da240fd54e4f361eab358287
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-12 09:36:04 -0300

    4.25. Modeling and implementing resources update with PUT

--- src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
+++ src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
@@ -2,12 +2,14 @@ package br.gabriel.springrestspecialist.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,5 +44,19 @@ public class KitchenController {
     @ResponseStatus(HttpStatus.CREATED)
     public Kitchen save(@RequestBody Kitchen kitchen) {
     	return repository.save(kitchen);
+    }
+    
+    @PutMapping("{id}")
+    public ResponseEntity<Kitchen> save(@PathVariable Integer id, @RequestBody Kitchen kitchen) {
+    	Kitchen current = repository.findById(id);
+    	
+    	if (current == null) {
+        	return ResponseEntity.notFound().build();
+        }
+    	
+    	BeanUtils.copyProperties(kitchen, current, "id");
+    	current = repository.save(current);
+    	
+    	return ResponseEntity.ok(current);
     }
 }
