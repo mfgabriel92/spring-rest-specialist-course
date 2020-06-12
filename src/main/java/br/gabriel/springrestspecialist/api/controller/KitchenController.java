6bd4b718e377756fc3fcee25951eee3223a1374f
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-11 21:44:42 -0300

    4.23. Modeling and implementing the inclusion of POST resources

--- src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
+++ src/main/java/br/gabriel/springrestspecialist/api/controller/KitchenController.java
@@ -3,10 +3,14 @@ package br.gabriel.springrestspecialist.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gabriel.springrestspecialist.domain.model.Kitchen;
@@ -32,5 +36,11 @@ public class KitchenController {
         }
         
         return ResponseEntity.ok(kitchen);
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Kitchen save(@RequestBody Kitchen kitchen) {
+    	return repository.save(kitchen);
     }
 }
