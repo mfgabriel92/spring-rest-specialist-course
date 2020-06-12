aa0a3a1f013508fc94a3e23375c647253b2a4bd5
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-12 20:51:48 -0300

    4.31. Challenge: modeling and implementing the update of restaurant resources

--- src/main/java/br/gabriel/springrestspecialist/api/controller/RestaurantController.java
+++ src/main/java/br/gabriel/springrestspecialist/api/controller/RestaurantController.java
@@ -2,12 +2,14 @@ package br.gabriel.springrestspecialist.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +49,24 @@ public class RestaurantController {
 		try {
 			restaurant = service.save(restaurant);
 			return ResponseEntity.status(HttpStatus.CREATED).body(restaurant);
+		} catch (ResourceNotFoundExeption e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> save(@PathVariable Integer id, @RequestBody Restaurant restaurant) {
+		try {
+			Restaurant current = repository.findById(id);
+			
+			if (current == null) {
+				return ResponseEntity.notFound().build();
+			}
+			
+			BeanUtils.copyProperties(restaurant, current, "id");
+			current = service.save(current);
+			
+			return ResponseEntity.ok().body(current);
 		} catch (ResourceNotFoundExeption e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
