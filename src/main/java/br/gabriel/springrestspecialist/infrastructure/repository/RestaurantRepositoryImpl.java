f4e87d02472a8c8fc7888071c63fc13674c0739b
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-13 17:37:50 -0300

    5.14. Adding restrictions in the where clause with Criteria API

--- src/main/java/br/gabriel/springrestspecialist/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/br/gabriel/springrestspecialist/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -21,8 +23,12 @@ public class RestaurantRepositoryImpl implements CustomRestaurantRepository {
 	public List<Restaurant> findBetweenShippingFees(BigDecimal minFee, BigDecimal maxFee) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
+		Root<Restaurant> root = criteria.from(Restaurant.class);
 		
-		criteria.from(Restaurant.class);
+		Predicate minFeePredicate = builder.greaterThanOrEqualTo(root.get("shippingFee"), minFee);
+		Predicate maxFeePredicate = builder.lessThanOrEqualTo(root.get("shippingFee"), maxFee);
+		
+		criteria.where(minFeePredicate, maxFeePredicate);
 		
 		return manager.createQuery(criteria).getResultList();
 	}
