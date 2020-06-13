638860772f9cc76c61bc279f938ad046a7e3fceb
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-13 16:30:30 -0300

    5.12. Implementing a dynamic query with JPQL

--- src/main/java/br/gabriel/springrestspecialist/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/br/gabriel/springrestspecialist/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -1,10 +1,12 @@
 package br.gabriel.springrestspecialist.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
@@ -17,11 +19,25 @@ public class RestaurantRepositoryImpl implements CustomRestaurantRepository {
 	
 	@Override
 	public List<Restaurant> findBetweenShippingFees(BigDecimal minFee, BigDecimal maxFee) {
-		String jpql = "FROM Restaurant WHERE shippingFee BETWEEN :minFee and :maxFee";
+		StringBuilder jpql = new StringBuilder();
+		jpql.append("FROM Restaurant WHERE 0 = 0 ");
 		
-		return manager.createQuery(jpql, Restaurant.class)
-			.setParameter("minFee", minFee)
-			.setParameter("maxFee", maxFee)
-			.getResultList();
+		HashMap<String, Object> params = new HashMap<>();
+		
+		if (minFee != null) {
+			jpql.append("AND shippingFee >= :minFee ");
+			params.put("minFee", minFee);
+		}
+		
+		if (maxFee != null) {
+			jpql.append("AND shippingFee <= :maxFee ");
+			params.put("maxFee", maxFee);
+		}
+		
+		TypedQuery<Restaurant> query = manager.createQuery(jpql.toString(), Restaurant.class);
+		
+		params.forEach((k, v) -> query.setParameter(k, v));
+		
+		return query.getResultList();
 	}
 }
