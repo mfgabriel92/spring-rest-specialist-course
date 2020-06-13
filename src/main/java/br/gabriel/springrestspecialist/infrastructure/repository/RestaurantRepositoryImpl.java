bf89baba6513b12bcfd5279e53eeaccdf7974cfc
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-13 18:00:49 -0300

    5.15. Making the query with Criteria API having dynamic filters

--- src/main/java/br/gabriel/springrestspecialist/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/br/gabriel/springrestspecialist/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
@@ -22,13 +21,17 @@ public class RestaurantRepositoryImpl implements CustomRestaurantRepository {
 	@Override
 	public List<Restaurant> findBetweenShippingFees(BigDecimal minFee, BigDecimal maxFee) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		
 		CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
 		Root<Restaurant> root = criteria.from(Restaurant.class);
 		
-		Predicate minFeePredicate = builder.greaterThanOrEqualTo(root.get("shippingFee"), minFee);
-		Predicate maxFeePredicate = builder.lessThanOrEqualTo(root.get("shippingFee"), maxFee);
+		if (minFee != null) {
+			criteria.where(builder.greaterThanOrEqualTo(root.get("shippingFee"), minFee));
+		}
 		
-		criteria.where(minFeePredicate, maxFeePredicate);
+		if (maxFee != null) {
+			criteria.where(builder.lessThanOrEqualTo(root.get("shippingFee"), maxFee));
+		}
 		
 		return manager.createQuery(criteria).getResultList();
 	}
