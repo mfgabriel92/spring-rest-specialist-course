7cc4372cf3f36e1373cfb62eb98e09179e1d0cf3
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-14 10:26:55 -0300

    6.7. Challenge: mapping many-to-one relationship

---
+++ src/main/java/br/gabriel/springrestspecialist/domain/model/Product.java
@@ -0,0 +1,42 @@
+package br.gabriel.springrestspecialist.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "products")
+public class Product {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String description;
+	
+	@Column(nullable = false)
+	private BigDecimal price;
+	
+	@Column(nullable = false)
+	private boolean active;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurant restaurant;
+}
