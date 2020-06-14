cb6d348e456462bd7e99ff255a7b08e931448f7f
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-14 13:48:32 -0300

    6.12. Altering the fetching strategy to Lazy Loading

--- src/main/java/br/gabriel/springrestspecialist/domain/model/Address.java
+++ src/main/java/br/gabriel/springrestspecialist/domain/model/Address.java
@@ -2,6 +2,7 @@ package br.gabriel.springrestspecialist.domain.model;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -25,7 +26,7 @@ public class Address {
 	@Column(name = "address_neighborhood")
 	private String neighborhood;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "address_city_id")
 	private City city;
 }
