0c30ae03e5492866f25580e6fb1004325c2cd47b
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-24 14:49:35 -0300

    13.16. Adding filtering to daily sales

--- src/main/java/br/gabriel/springrestspecialist/infrastructure/service/OrderQueryServiceImpl.java
+++ src/main/java/br/gabriel/springrestspecialist/infrastructure/service/OrderQueryServiceImpl.java
@@ -1,5 +1,7 @@
 package br.gabriel.springrestspecialist.infrastructure.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -9,12 +11,14 @@ import javax.persistence.criteria.CompoundSelection;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
 import br.gabriel.springrestspecialist.domain.filter.DailySalesFilter;
 import br.gabriel.springrestspecialist.domain.model.Order;
+import br.gabriel.springrestspecialist.domain.model.OrderStatus;
 import br.gabriel.springrestspecialist.domain.model.aggregate.DailySales;
 import br.gabriel.springrestspecialist.domain.service.OrderQueryService;
 
@@ -28,6 +32,7 @@ public class OrderQueryServiceImpl implements OrderQueryService {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<DailySales> query = builder.createQuery(DailySales.class);
         Root<Order> root = query.from(Order.class);
+        List<Predicate> predicates = new ArrayList<>();
         
         Expression<Date> functionDateCratedAt = builder.function("DATE", Date.class, root.get("createdAt"));
         
@@ -38,7 +43,18 @@ public class OrderQueryServiceImpl implements OrderQueryService {
             builder.sum(root.get("grandTotal"))
         );
         
+        if (filter.getRestaurantId() != null) {
+            predicates.add(builder.equal(root.get("restaurant"), filter.getRestaurantId()));
+        } else if (filter.getInitialDate() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("createdAt"), filter.getInitialDate()));
+        } else if (filter.getEndingDate() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("createdAt"), filter.getEndingDate()));
+        }
+        
+        predicates.add(root.get("status").in(Arrays.asList(OrderStatus.CONFIRMED, OrderStatus.DELIVERED)));
+        
         query.select(select);
+        query.where(builder.and(predicates.toArray(new Predicate[0])));
         query.groupBy(functionDateCratedAt);
         
         return entityManager.createQuery(query).getResultList();
