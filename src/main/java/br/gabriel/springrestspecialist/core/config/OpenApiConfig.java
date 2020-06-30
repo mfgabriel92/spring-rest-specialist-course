700b252f2f0dc8d2f4da459fa0b1de5b94056359
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-06-30 10:43:13 -0300

    18.4. Generating the API documentation in HTML with Swagger UI and SpringFox

--- src/main/java/br/gabriel/springrestspecialist/core/config/OpenApiConfig.java
+++ src/main/java/br/gabriel/springrestspecialist/core/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package br.gabriel.springrestspecialist.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,12 +12,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class OpenApiConfig {
+public class OpenApiConfig implements WebMvcConfigurer {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
                 .apis(RequestHandlerSelectors.any())
                 .build();
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+        
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
