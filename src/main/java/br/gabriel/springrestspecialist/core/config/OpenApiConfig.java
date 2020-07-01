c8bdee6ad836e572f2f81ffd43562521812a3c1a
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-01 20:34:13 -0300

    18.6. Describing API information in the documentation

--- src/main/java/br/gabriel/springrestspecialist/core/config/OpenApiConfig.java
+++ src/main/java/br/gabriel/springrestspecialist/core/config/OpenApiConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +20,8 @@ public class OpenApiConfig implements WebMvcConfigurer {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
                 .apis(RequestHandlerSelectors.basePackage("br.gabriel.springrestspecialist.api"))
-                .build();
+                .build()
+            .apiInfo(apiInfo());
     }
     
     @Override
@@ -28,5 +31,13 @@ public class OpenApiConfig implements WebMvcConfigurer {
         
         registry.addResourceHandler("/webjars/**")
             .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Spring Rest Specialist")
+            .description("Spring Rest Specialist course API")
+            .version("0.0.1")
+            .build();
     }
 }
