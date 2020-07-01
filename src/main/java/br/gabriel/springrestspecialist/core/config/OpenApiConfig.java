7f1df030f54db6a1e30292d03c3ca0ce6ca38f1c
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-01 20:29:53 -0300

    18.5. Selecting the endpoints from the API to generate the documentation

--- src/main/java/br/gabriel/springrestspecialist/core/config/OpenApiConfig.java
+++ src/main/java/br/gabriel/springrestspecialist/core/config/OpenApiConfig.java
@@ -17,7 +17,7 @@ public class OpenApiConfig implements WebMvcConfigurer {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.gabriel.springrestspecialist.api"))
                 .build();
     }
     
