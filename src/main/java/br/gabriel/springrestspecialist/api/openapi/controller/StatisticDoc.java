cf564ea7a2dd89ed973a1b5402f4002fa6e62c23
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-04 13:56:33 -0300

    18.40. Challenge: describing the documentation of daily sales endpoint

--- src/main/java/br/gabriel/springrestspecialist/api/openapi/controller/StatisticDoc.java
+++ src/main/java/br/gabriel/springrestspecialist/api/openapi/controller/StatisticDoc.java
@@ -5,10 +5,17 @@ import java.util.List;
 import br.gabriel.springrestspecialist.domain.filter.DailySalesFilter;
 import br.gabriel.springrestspecialist.domain.model.aggregate.DailySales;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 @Api(tags = "Statistic")
 public interface StatisticDoc {
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "restaurantId", value = "The restaurant ID", dataType = "int", example = "1"),
+        @ApiImplicitParam(name = "initialDate", value = "The initial date", dataType = "date-time", example = "2020-07-07T00:00:00Z"),
+        @ApiImplicitParam(name = "endingDate", value = "The ending date", dataType = "date-time", example = "2020-07-08T00:00:00Z"),
+    })
     @ApiOperation("List all the daily sales for a period")
     List<DailySales> dailySales(DailySalesFilter filter);
 }
--- src/main/java/br/gabriel/springrestspecialist/domain/filter/OrderFilter.java
+++ src/main/java/br/gabriel/springrestspecialist/domain/filter/OrderFilter.java
@@ -5,24 +5,19 @@ import java.time.OffsetDateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class OrderFilter {
-    @ApiModelProperty(value = "The user ID", example = "1")
     private Integer userId;
     
-    @ApiModelProperty(value = "The restaurant ID", example = "1")
     private Integer restaurantId;
     
-    @ApiModelProperty(value = "The initial date", example = "2020-07-07T00:00:00Z")
     @DateTimeFormat(iso = ISO.DATE_TIME)
     private OffsetDateTime initialDate;
     
-    @ApiModelProperty(value = "The ending date", example = "2020-07-08T00:00:00Z")
     @DateTimeFormat(iso = ISO.DATE_TIME)
     private OffsetDateTime endingDate;
 }
