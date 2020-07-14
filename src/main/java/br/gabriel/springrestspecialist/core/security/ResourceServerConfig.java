521d301bf8bcacdc48415a60917646b57ed15b69
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-14 20:13:35 -0300

    22.4. Configuring user authentication in memory

--- src/main/java/br/gabriel/springrestspecialist/core/security/ResourceServerConfig.java
+++ src/main/java/br/gabriel/springrestspecialist/core/security/ResourceServerConfig.java
@@ -1,25 +1,43 @@
 package br.gabriel.springrestspecialist.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.
-			httpBasic().and()
-				.authorizeRequests()
-					.antMatchers(HttpMethod.POST, "/v1/users/**").permitAll()
-					.anyRequest().authenticated().and()
-				.sessionManagement()
-					.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.cors().and()
-				.csrf().disable();
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.
+            inMemoryAuthentication()
+                .withUser("admin")
+                    .password(passwordEncoder().encode("admin"))
+                    .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+            httpBasic().and()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/v1/users/**").permitAll()
+                    .anyRequest().authenticated().and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .cors().and()
+                .csrf().disable();
+    }
+    
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
