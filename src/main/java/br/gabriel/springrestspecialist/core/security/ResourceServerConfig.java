5ea295211183fb18015fbe94e212a7fdde9b7046
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-18 12:18:18 -0300

    23.6. Configuring the resource server for JWT signed with asymmetric key

--- src/main/java/br/gabriel/springrestspecialist/core/security/ResourceServerConfig.java
+++ src/main/java/br/gabriel/springrestspecialist/core/security/ResourceServerConfig.java
@@ -1,14 +1,23 @@
 package br.gabriel.springrestspecialist.core.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+    @Value("${srs.jwt.signing-key}")
+    private String signingKey;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -17,6 +26,13 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated().and()
             .cors().and()
             .csrf().disable()
-            .oauth2ResourceServer().opaqueToken();
+            .oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    protected JwtDecoder jwtDecoder() {
+        System.out.println(signingKey);
+        SecretKeySpec keySpec = new SecretKeySpec(signingKey.getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(keySpec).build();
     }
 }
