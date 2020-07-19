e9a1ed01d9b8c33721556b6ba848ca94f8b89b95
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-18 22:10:37 -0300

    23.12. Configuring JWT validation with the public key on the resource server

--- src/main/java/br/gabriel/springrestspecialist/core/security/ResourceServerConfig.java
+++ src/main/java/br/gabriel/springrestspecialist/core/security/ResourceServerConfig.java
@@ -1,23 +1,14 @@
 package br.gabriel.springrestspecialist.core.security;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
-    @Value("${srs.jwt.signing-key}")
-    private String signingKey;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -27,12 +18,5 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
             .cors().and()
             .csrf().disable()
             .oauth2ResourceServer().jwt();
-    }
-
-    @Bean
-    protected JwtDecoder jwtDecoder() {
-        System.out.println(signingKey);
-        SecretKeySpec keySpec = new SecretKeySpec(signingKey.getBytes(), "HmacSHA256");
-        return NimbusJwtDecoder.withSecretKey(keySpec).build();
     }
 }
