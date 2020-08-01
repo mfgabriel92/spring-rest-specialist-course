c50cb6875f008671306e60a423009b6af9fa2d41
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-08-01 18:33:15 -0300

    19.36. Implementing API's root entry point

--- src/main/java/br/gabriel/springrestspecialist/api/v1/controller/PermissionController.java
+++ src/main/java/br/gabriel/springrestspecialist/api/v1/controller/PermissionController.java
@@ -6,12 +6,13 @@ import br.gabriel.springrestspecialist.api.v1.openapi.controller.PermissionDoc;
 import br.gabriel.springrestspecialist.domain.repository.PermissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/permissions")
+@RequestMapping(value = "/v1/permissions", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PermissionController implements PermissionDoc {
     @Autowired
     private PermissionRepository repository;
---
+++ src/main/java/br/gabriel/springrestspecialist/api/v1/controller/RootEntryPointController.java
@@ -0,0 +1,32 @@
+package br.gabriel.springrestspecialist.api.v1.controller;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RootEntryPointController {
+    @GetMapping
+    public RootEntryPointModel root() {
+        RootEntryPointModel rootEntryPointModel = new RootEntryPointModel();
+    
+        rootEntryPointModel.add(linkTo(CityController.class).withRel("cities"));
+        rootEntryPointModel.add(linkTo(CuisineController.class).withRel("cuisines"));
+        rootEntryPointModel.add(linkTo(GroupController.class).withRel("groups"));
+        rootEntryPointModel.add(linkTo(OrderController.class).withRel("orders"));
+        rootEntryPointModel.add(linkTo(PaymentMethodController.class).withRel("payment-methods"));
+        rootEntryPointModel.add(linkTo(PermissionController.class).withRel("permissions"));
+        rootEntryPointModel.add(linkTo(RestaurantController.class).withRel("restaurants"));
+        rootEntryPointModel.add(linkTo(StateController.class).withRel("states"));
+        rootEntryPointModel.add(linkTo(UserController.class).withRel("users"));
+    
+        return rootEntryPointModel;
+    }
+    
+    private static class RootEntryPointModel extends RepresentationModel<RootEntryPointModel> {}
+}
